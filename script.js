626baa074c79a39eb7b5efdbe42a1b17d9c68025
sometimemecry <songphon.yy@hotmail.com>
2023-03-09 15:11:51 +0700

    Test

---
+++ script.js
@@ -0,0 +1,6 @@
+const mytext = document.getElementById("text");
+function myFunction() {
+    alert("Hello World!");
+    console.log("The button has been clicked");
+    text.innerHTML = "The button has been clicked";
+}
