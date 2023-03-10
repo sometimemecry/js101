03968d26a79261751cb34a20d89e963cf67badf5
sometimemecry <songphon.yy@hotmail.com>
2023-03-10 14:36:40 +0700

    JS101 Lesson

---
+++ script1.js
@@ -0,0 +1,29 @@
+// console.log("Hello world!");
+
+// let email = 'songphon2210@hotmail.com';
+// console.log(email);
+
+// ----Array Lesson---- //
+
+// let computerDevice = ['monitor', 'mouse', 'keyboard',];
+// console.log(computerDevice[1]);
+// computerDevice[3] = 'vga';
+// console.log(computerDevice);
+// computerDevice[4] = true;
+// console.log(computerDevice);
+// console.log(typeof computerDevice[4]);
+
+
+// ----Object Lesson ---- //
+
+// let user = {
+//     firstName: 'Songphon',
+//     lastName: 'Yuenyaw',
+//     email: 'songphon2210@hotmail.com',
+//     favoriteFood: ['Somtam', 'Pepsi', 'Cola'],
+// }
+
+// console.log(user);
+// console.log("User's name : " + user.firstName);
+// console.log(user.favoriteFood[1]);
+
